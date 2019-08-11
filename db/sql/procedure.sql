6249a50605937358c517dd61a523bf2eab612590
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-08-11 23:37:01 +0900

    memo

--- db/sql/procedure.sql
+++ db/sql/procedure.sql
@@ -2,7 +2,7 @@ delimiter //
 create procedure datainsert()
 begin
 declare i int default 0;
-while i<1000 do
+while i<30000 do
   set @sql="insert posts (topic_id, name, body) values (1, '鈴木 太郎', 'data');";
 prepare stmt from @sql;
 execute stmt;
