3dbc40d0357d33edba1e46f5fe994f186258e92c
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-08-18 17:32:47 +0900

    コメント記載

--- ts_express/src/routes/topic/index.ts
+++ ts_express/src/routes/topic/index.ts
@@ -9,7 +9,9 @@ const router = Router();
 // topic一覧を返却
 // router.get('/:id', (req, res: Response, next) => {
 router.get('/:id', (req: PostForm, res: Response, next: NextFunction) => {
-    console.log(req.params.id)
+    // ここなぜかコンパイルエラーになるからコメント化
+    // console.log(req.params.id)
+    // この方法もなぜかデータが格納されない
     console.log(req.id);
     const posts = Post.getPostsByTopicId(Number(req.id), Number(req.query.page));
 
