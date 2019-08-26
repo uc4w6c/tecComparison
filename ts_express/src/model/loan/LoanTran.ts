1024c6e0541ae575984e507704961cad83c87c79
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-08-26 17:17:33 +0900

    ローン計算完成

--- ts_express/src/model/loan/LoanTran.ts
+++ ts_express/src/model/loan/LoanTran.ts
@@ -1,6 +1,6 @@
 import { Rate } from '../../infrastructure/loan/Rate';
 import { balancesMinus } from './Balance';
-import moment from 'moment'
+import * as moment from 'moment';
 
 export class LoanTran {
     private id: number;
@@ -11,11 +11,11 @@ export class LoanTran {
     private balance: number;
 
     constructor(id: number,
-        mouth: string,
-        total: number,
-        principal: number,
-        interest: number,
-        balance: number) {
+                mouth: string,
+                total: number,
+                principal: number,
+                interest: number,
+                balance: number) {
 
         this.id = id;
         this.mouth = mouth;
@@ -25,8 +25,23 @@ export class LoanTran {
         this.balance = balance;
     }
 
+    public getId(): number {
+        return this.id;
+    }
+    public getMouth(): string {
+        return this.mouth;
+    }
+    public getTotal(): number {
+        return this.total;
+    }
     public getPrincipal(): number {
         return this.principal;
+    }
+    public getInterest(): number {
+        return this.interest;
+    }
+    public getBalance(): number {
+        return this.balance;
     }
 }
 
@@ -39,72 +54,70 @@ export const calcLoan = (borrowingAmount: number, repaymentPeriod: number): Loan
     let rateInterface: RateInterface;
     rateInterface = new Rate();
     const interestRate = rateInterface.findRate();
-    const fixedRepaymentAmount = fixedRepaymentAmountCalc(borrowingAmount, interestRate, repaymentPeriod)
+    const fixedRepaymentAmount = fixedRepaymentAmountCalc(borrowingAmount, interestRate, repaymentPeriod);
 
     const repaymentMouthCount = repaymentMouthCountCalc(repaymentPeriod);
-	let balanceMinus = balancesMinus(borrowingAmount);
-	let balance = borrowingAmount;
-	let date = new Date();
-    let loanTrans: LoanTran[] = [];
-    for (let i:number = 1; i < repaymentMouthCount + 1; i++) {
-        const loanTran = buildLoanTran(i, moment(date).format("YYYY/MM"), fixedRepaymentAmount, interestRate, balance);
+    const balanceMinus = balancesMinus(borrowingAmount);
+    let balance = borrowingAmount;
+    const date = new Date();
+    const loanTrans: LoanTran[] = [];
+    for (let i:number = 1; i < repaymentMouthCount + 1; i += 1) {
+        const loanTran = buildLoanTran(i, moment(date).format('YYYY/MM'), fixedRepaymentAmount, interestRate, balance);
         loanTrans.push(loanTran);
-		date.setMonth( date.getDate() + 1);
-		balance = balanceMinus(loanTran.getPrincipal())
+        date.setMonth(date.getMonth() + 1);
+        balance = balanceMinus(loanTran.getPrincipal());
     }
 
-    return loanTrans
-}
+    return loanTrans;
+};
 
-const buildLoanTran = (id: number, month: string, total: number, 
+const buildLoanTran = (id: number, month: string, total: number,
                        interestRate: number, beforeBalance: number) : LoanTran => {
 
-        const interest = calcMonthInterestCalc(beforeBalance, interestRate);
-        const principal = total - interest;
-        // 支払後残高が今回支払額より少ない場合は最終支払分と判断しまとめる
-        let lastBalance = 0;
-        if (beforeBalance-principal-principal < 0) {
-            lastBalance = beforeBalance - principal;
-        }
+    const interest = calcMonthInterestCalc(beforeBalance, interestRate);
+    const principal = total - interest;
+    // 支払後残高が今回支払額より少ない場合は最終支払分と判断しまとめる
+    let lastBalance = 0;
+    if (beforeBalance - principal - principal < 0) {
+        lastBalance = beforeBalance - principal;
+    }
 
-        return new LoanTran(id,                                         // id
-                            month,                                      // month
-                            total + lastBalance,                        // total
-                            principal + lastBalance,                    // principal
-                            interest,                                   // interest
-                            beforeBalance - principal - lastBalance,    // balance
-        );
-    
-}
-
+    return new LoanTran(id,                                         // id
+                        month,                                      // month
+                        total + lastBalance,                        // total
+                        principal + lastBalance,                    // principal
+                        interest,                                   // interest
+                        beforeBalance - principal - lastBalance,    // balance
+    );
+};
 
 // 本来ここに記載するものではないが、簡略化のため
-const RoundDown = (num: number, place: number) : number => {
-	const shift = Math.pow(10, place);
-	return Math.floor(num*shift) / shift;
-}
+const roundDown = (num: number, place: number) : number => {
+    const shift = Math.pow(10, place);
+    return Math.floor(num * shift) / shift;
+};
 
 // 年利から月ごとの金利を計算
 const mouthRateCalc = (interestRate: number): number => {
-    return RoundDown(interestRate/100/12, 10);
-}
+    return roundDown(interestRate / 100 / 12, 10);
+};
 
 // 年数から支払回数を計算
 const repaymentMouthCountCalc = (repaymentPeriod: number) : number => {
-	return repaymentPeriod * 12;
-}
+    return repaymentPeriod * 12;
+};
 
 // 現在の残高と年利から今月の支払い利息を計算
 const calcMonthInterestCalc = (balance: number, interestRate: number) : number => {
-	return RoundDown(balance*mouthRateCalc(interestRate), 0);
-}
+    return roundDown(balance * mouthRateCalc(interestRate), 0);
+};
 
 // 毎月の一定返済金額を計算する
 const fixedRepaymentAmountCalc = (balance: number, interestRate: number, repaymentPeriod:number) : number => {
     const repaymentMouthCount = repaymentMouthCountCalc(repaymentPeriod);
     const mouthRate = mouthRateCalc(interestRate);
 
-    const numerator = RoundDown(balance * mouthRate * (Math.pow((1 + mouthRate), repaymentMouthCount)), 10);
-    const denominator = RoundDown(Math.pow((1 + mouthRate), repaymentMouthCount) -1, 10);
-    return RoundDown(numerator / denominator, 0)
-}
+    const numerator = roundDown(balance * mouthRate * (Math.pow((1 + mouthRate), repaymentMouthCount)), 10);
+    const denominator = roundDown(Math.pow((1 + mouthRate), repaymentMouthCount) - 1, 10);
+    return roundDown(numerator / denominator, 0);
+};
--- ts_express/src/routes/loan/index.ts
+++ ts_express/src/routes/loan/index.ts
@@ -1,13 +1,13 @@
 // import * as Express from 'express';
 import { Router, Request, Response, NextFunction } from 'express';
-import { LoanTran, calcLoan } from '../../model/loan/LoanTran';
+import { calcLoan } from '../../model/loan/LoanTran';
 
 const router = Router();
 
 // 元利均等ローン計算
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    const borrowingAmount = 35000000
-	const repaymentPeriod = 35
+    const borrowingAmount = 35000000;
+    const repaymentPeriod = 35;
     const loanTrans = calcLoan(borrowingAmount, repaymentPeriod);
 
     res.status(200).json(loanTrans);
